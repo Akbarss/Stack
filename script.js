48855f774cbfeb8446082824e78b9028216756cd
Akbarss <kbrgulyamov@gmail.com>
2023-11-21 15:33:19 +0500

    feat: stack v2 add

--- script.js
+++ script.js
@@ -1,43 +1,3 @@
-  // class Stack {
-  //   constructor() {
-  //     this.items = [];
-  //   }
-
-  //   push(element) {
-  //     this.items.push(element);
-  //   }
-
-  //   pop() {
-  //     if (this.items.length === 0) {
-  //       return "Stack Empty";
-  //     }
-  //     return this.items.pop();
-  //   }
-
-  //   peek() {
-  //     return this.items[this.items.length - 1];
-  //   }
-
-  //   isEmpty() {
-  //     return this.items.length === 0;
-  //   }
-
-  //   size() {
-  //     return this.items.length;
-  //   }
-  // }
-
-  // let myStack = new Stack();
-  // myStack.push(1);
-  // myStack.push(2);
-  // myStack.push(3);
-
-  // console.log("Вершина стека:", myStack.peek());
-  // console.log("Размер стека:", myStack.size());
-
-  // myStack.pop();
-  // console.log("Стек после удаления элемента:", myStack.items);
-
 //---- Stack V2 ------
 
 // Создаем класс Stack
@@ -88,31 +48,10 @@ let myStack2 = new Stack();
 myStack2.push(1);
 myStack2.push(2);
 myStack2.push(3);
-myStack2.push(40)
+myStack2.push(40);
 
 console.log("Вершина стека:", myStack2.peek());
 console.log("Размер стека:", myStack2.size());
 
 myStack2.pop();
 console.log("Стек после удаления элемента:", myStack2.items);
-
-// class Stack {
-//   constructor() {
-//     this.items = [];
-//     this.count = 0;
-//   }
-
-//   // Metot Push
-//   push(el) {
-//     this.items[this.count] = el;
-//     console.log(`${el} added to ${this.count}`)
-//     this.count += 1;
-//     return this.count - 1;
-//   }
-// }
-
-// const stack = new Stack();
-
-// stack.push(100);
-// stack.push(200);
-// stack.push(300);
